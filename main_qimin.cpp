38be6658f1c5b70b4d3512bba09383b8e0760752
Qimin Yao <yaoqimin@98.195.21.198.tigernet.wifi.dyn.clemson.edu>
2013-02-23 14:51:19 -0500

    add main_qimin.cpp

---
+++ main_qimin.cpp
@@ -0,0 +1,16 @@
+#include "graphic_object.h"
+
+/* The code is used to test Qimin's ReadFile code */
+int main(int argc, char** argv) {
+  GraphicObject* ob = new GraphicObject();
+  ob->readFile();
+  ob->test();
+  vector<Property> test_v = ob->getProperties();
+  vector<ElementFace> test_f = ob->getFaces();
+  cout << "this is a vertex test: " << test_v[0].x << endl;
+  cout << "this is a face test: " << test_f[0].y << endl;
+  ob->calculateVectors();
+  ob->test();
+  delete ob;
+  return 0;
+}
