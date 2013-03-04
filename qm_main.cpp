84ae7adce0b222473eb7f6640b4a0f8c8114619c
Julian Wu <cosmobserver@gmail.com>
2013-03-04 02:42:11 -0500

    Clean up useless files

--- qm_main.cpp
+++
@@ -1,9 +0,0 @@
-#include "GraphicObject.h"
-
-/* The code is used to test Qimin's ReadFile code */
-int main(int argc, char** argv) {
-  GraphicObject* ob = new GraphicObject();
-  ob->execute();
-  delete ob;
-  return 0;
-}
